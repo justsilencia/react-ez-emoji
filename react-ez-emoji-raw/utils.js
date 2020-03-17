6a7d3a43349b61ffeeaff2b79ef2f9e0903b122f
Jay <48294438+justsilencia@users.noreply.github.com>
2020-03-17 13:10:41 -0400

    Updated saveSelection and restoreSelection methods
    Needed for compatibility with webkit (chrome) browsers.

--- react-ez-emoji-raw/utils.js
+++ react-ez-emoji-raw/utils.js
@@ -2,73 +2,40 @@ import { emojiIcons } from './emoji-icons';
 import React from 'react';
 
 const saveSelection = (function() {
-    if (window.getSelection && document.createRange) {
-        return function(containerEl) {
-            var range = window.getSelection().getRangeAt(0);
-            var preSelectionRange = range.cloneRange();
-            preSelectionRange.selectNodeContents(containerEl);
-            preSelectionRange.setEnd(range.startContainer, range.startOffset);
-            var start = preSelectionRange.toString().length;
-    
-            return {
-                start: start,
-                end: start + range.toString().length
+    if (window.getSelection) {
+        return function() {
+            var sel = window.getSelection(), ranges = [];
+            if (sel.rangeCount) {
+                for (var i = 0, len = sel.rangeCount; i < len; ++i) {
+                    ranges.push(sel.getRangeAt(i));
+                }
             }
-        }
-    } else if (document.selection && document.body.createTextRange) {
-        return function(containerEl) {
-            var selectedTextRange = document.selection.createRange();
-            var preSelectionTextRange = document.body.createTextRange();
-            preSelectionTextRange.moveToElementText(containerEl);
-            preSelectionTextRange.setEndPoint("EndToStart", selectedTextRange);
-            var start = preSelectionTextRange.text.length;
-    
-            return {
-                start: start,
-                end: start + selectedTextRange.text.length
-            }
-        }
+            return ranges;
+        };
+    } else if (document.selection && document.selection.createRange) {
+        return function() {
+            var sel = document.selection;
+            return (sel.type.toLowerCase() !== 'none') ? sel.createRange() : null;
+        };
     }
 })();
 
 const restoreSelection = (function() {
-        if (window.getSelection && document.createRange) {
-            return function(containerEl, savedSel) {
-                    var charIndex = 0, range = document.createRange();
-                    range.setStart(containerEl, 0);
-                    range.collapse(true);
-                    var nodeStack = [containerEl], node, foundStart = false, stop = false;
-                
-                    while (!stop && (node = nodeStack.pop())) {
-                        if (node.nodeType === 3) {
-                            var nextCharIndex = charIndex + node.length;
-                            if (!foundStart && savedSel.start >= charIndex && savedSel.start <= nextCharIndex) {
-                                range.setStart(node, savedSel.start - charIndex);
-                                foundStart = true;
-                            }
-                            if (foundStart && savedSel.end >= charIndex && savedSel.end <= nextCharIndex) {
-                                range.setEnd(node, savedSel.end - charIndex);
-                                stop = true;
-                            }
-                            charIndex = nextCharIndex;
-                        } else {
-                            var i = node.childNodes.length;
-                            while (i--) {
-                                nodeStack.push(node.childNodes[i]);
-                            }
-                        }
-                    }
+    if (window.getSelection) {
+        return function(savedSelection) {
+            var sel = window.getSelection();
+            sel.removeAllRanges();
+            for (var i = 0, len = savedSelection.length; i < len; ++i) {
+                sel.addRange(savedSelection[i]);
             }
-        } else if (document.selection && document.body.createTextRange) { 
-            return function(containerEl, savedSel) {
-                    var textRange = document.body.createTextRange();
-                    textRange.moveToElementText(containerEl);
-                    textRange.collapse(true);
-                    textRange.moveEnd("character", savedSel.end);
-                    textRange.moveStart("character", savedSel.start);
-                    textRange.select();
+        };
+    } else if (document.selection && document.selection.createRange) {
+        return function(savedSelection) {
+            if (savedSelection) {
+                savedSelection.select();
             }
-        }
+        };
+    }
 })();
 
 const htmlEntities = function(str) {
